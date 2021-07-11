a05b242ba163f4c2e2d933d8fe9218d8c22af69a
Maslah Ali <alimm6@augusburg.edu>
2021-07-11 08:48:46 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+pyrimid = 10
+for i in range(0,pyrimid):
+    for k in range(0,pyrimid-i):
+
+        print(" ",end="")
+    for j in range(0, 2*i+1):
+        print("*", end="")
+    print("Somalia")
